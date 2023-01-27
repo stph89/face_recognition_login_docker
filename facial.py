da03691f32ae11cd5c94e78d3179c4cd0e086340
yaya <penelope306115257@gmail.com>
2023-01-27 17:25:29 +0100

    update .gitignore

--- facial.py
+++ facial.py
@@ -1,42 +1,26 @@
 import face_recognition
 import cv2
 import numpy as np
+import os
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
-# Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+path = "info_facial"
+image = os.listdir(path)
+known_face_encodings = []
+known_face_names = []
 
-# Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-
-
-sebastian_image = face_recognition.load_image_file("know_people/sebastian.jpg")
-sebastian_face_encoding = face_recognition.face_encodings(sebastian_image)[0]
-
-# Create arrays of known face encodings and their names
-known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding,
-    sebastian_face_encoding
-]
-known_face_names = [
-    "Barack Obama",
-    "Joe Biden",
-    "Sebastian Pedro Degaudenci"
-]
+for a in image:
+    if a != ".DS_Store":
+        name_image = face_recognition.load_image_file(str(path) + "/" + str(a))
+        name_face_encoding = face_recognition.face_encodings(name_image)
+        if len(name_face_encoding) > 0:
+            known_face_names.append(a.strip('.jpg'))
+            # 获取检测到人脸时面部编码信息中第一个面部编码
+            name_face_encoding = name_face_encoding[0]
+            known_face_encodings.append(name_face_encoding)
+        else:
+            print("¡No se detectó un área de cara válida!")
 
 # Initialize some variables
 face_locations = []
