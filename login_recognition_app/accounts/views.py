35f058f5768a518a6e34e14e0e4182b2ed4286b3
Stephany Valderrama Carmona <svalderrama4@gmail.com>
2023-02-09 17:48:28 +0100

    Métodos de reconocimiento facial: registrar imagenes, encoding, verificacion de usuarios y hacer match con lo registrado

--- login_recognition_app/accounts/views.py
+++ login_recognition_app/accounts/views.py
@@ -12,11 +12,11 @@ from PIL import Image as img, ImageDraw
 import fnmatch
 import os
 
-# Used for finding dir paths
+# Instanciar la ruta del directorio
 # rootDir = '../facialrecognition'
 
-IMAGES_DIR = 'facialrecognition/media/images'
-VERIF_DIR = 'facialrecognition/media/verif'
+IMAGES_DIR = 'facialrecognition/media/images' # imágenes del login
+VERIF_DIR = 'facialrecognition/media/verif' #imagenes del registro
 
 user_photo = []
 user_photo_name = []
@@ -26,7 +26,7 @@ def home(request):
     return render(request, 'accounts/home.html')
 
 """Método login
-Paso 1: Solicitar user name
+Paso 1: Solicitar username
 Paso 2: Verificación de foto si username existe
 Paso 3: Mensaje de error si no existe"""
 
@@ -38,25 +38,30 @@ def login(request):
     if request.method == "POST":
         form = LoginForm(request.POST, request.FILES)
         if form.is_valid():
-            userName = form.cleaned_data.get("Nombre de Usuario")
+            userName = form.cleaned_data.get("username")
             userLoginName = userName
             users = UserProfile.objects.filter(
-                title=userLoginName)  # Si el nombre de usuario coincide con un nombre de la base de datos pasar a verificar foto
+                title=userLoginName)
             if users:
                 return render(request, 'accounts/verifyLogin.html', context)
+            #Si el nombre de usuario coincide con un nombre de la base de datos pasar a verificar foto
 
             else:
                 form = LoginForm()
-                errorMessage = "Error: invalid Username please try again"  # else return error message
+                errorMessage = "Error: Usuario aún no registrado."  # else return error message
                 context = {'form': form, 'errorMessage': errorMessage, }
                 return render(request, 'accounts/login.html', context)
+            # Si el nombre de usuario NO coincide con un nombre de la base de datos pasar a verificar foto
     else:
         form = LoginForm()
         context['form'] = form
         return render(request, 'accounts/login.html', context)
 
+"""Método register
+Paso 1: Solicitar username para registrar
+Paso 2: Subir imagen relacionada a este nuevo nombre de usuario
+Paso 3: Mensaje de error si el usuario ya existe"""
 
-# Fully working adding users and images
 def register(request):
     context = {}
     if request.method == "POST":
@@ -66,7 +71,7 @@ def register(request):
             img = form.cleaned_data.get("images")
             errorname = UserProfile.objects.filter(title=name)
             if errorname:
-                errorMessage = "Error: username already exists"  # else return error message
+                errorMessage = "Error: Este usuario ya existe."  # else return error message
                 context = {'form': form, 'errorMessage': errorMessage, }
                 return render(request, 'accounts/register.html', context)
             else:
@@ -83,16 +88,16 @@ def register(request):
     context['form'] = form
     return render(request, 'accounts/register.html', context)
 
-
+"""Método verifyPhoto
+Paso 1: Se recorren todas las imágenes del directorio y se añaden a las listas
+Paso 2: Se cargan las imágenes registradas en la base de datos
+Paso 3: Mensaje de error si no se toma la foto
+Paso 4: Si toma foto, hace el encoding, compara el encoding nuevo con el encoding 
+de las fotos del registro y hace match con los nombres."""
 def verifyPhoto(request):
     context = {}
-    # DEBUG CODE TO FIND DIRECTORY TREES
-    # for dirName, subdirList, fileList in os.walk(rootDir):
-    #     print('Found directory: %s' % dirName)
-    #     for fname in fileList:
-    #         print('\t%s' % fname)
 
-    '''Loop over images in directory, add to lists
+    '''Se recorren todas las imágenes del directorio y se añaden a las listas
     '''
     print("Loading registered faces database!")
     for file in os.listdir(IMAGES_DIR):
@@ -100,25 +105,23 @@ def verifyPhoto(request):
             image = face_recognition.load_image_file(f"{IMAGES_DIR}/{file}")
             name_face_encoding = face_recognition.face_encodings(image)
             if len(name_face_encoding) > 0:
-                # encodes all found faces
+                # encoding de todas las caras encontradas
                 encoding = name_face_encoding[0]
-                # add encodings to list
+                # se adicionan los encodings a la lista
                 user_photo.append(encoding)
-                # add photo name to list
+                # se adiciona el nombre de la foto a la lista
                 user_photo_name.append(file)
-
-                # DEBUG print
-                # print(user_photo)
-                # print(user_photo_name)
 
             else:
                 print("¡No se detectó un área de cara válida!")
                 pass
             continue
 
-    '''Decides where user is redirected based on return of takePhoto()
+    '''Aqui se decide a dónde se redigirá el ususario:
     if true - redirect to login
     if false - redirect to register
+    
+    Si se cumple o no el condicional de takePhotoLogin()
     '''
 
     if takePhotoLogin() is True:
@@ -134,39 +137,43 @@ def verifyPhoto(request):
 
         return redirect('/login')
 
+"""Método takePhotoLogin
+Paso 1: Se recorren todas las imágenes del directorio y se añaden a las listas
+Paso 2: Se cargan las imágenes registradas en la base de datos
+Paso 3: Mensaje de error si no se toma la foto"""
 def takePhotoLogin():
     context = {}
-    # Get user to take image
+    # Abrir la cámara para tomar la imagen
     capture = cv2.VideoCapture(0)
     # Create live feed to take image and save photo
     while True:
         ret, frame = capture.read()
         cv2.imshow("Take Verification picture", frame)
-        if (cv2.waitKey(1) & 0xFF == ord('q')):
+        if (cv2.waitKey(1) & 0xFF == ord('q')): #la foto se toma con la letra q
             cv2.imwrite(f"{VERIF_DIR}/photo.png", frame)
             break
     capture.release()
     cv2.destroyAllWindows()
     cv2.waitKey(1)
-    # load the image
+    # Se carga la imagen tomada
     ver_photo = face_recognition.load_image_file(f"{VERIF_DIR}/photo.png")
-    # encode the image
+    # Se hace el encoding de la imagen
     ver_photo_enc = face_recognition.face_encodings(ver_photo)[0]
 
     # DEBUG print
     print(f"VERIFICATION PHOTO: {ver_photo}")
     print(f"VERIFICATION PHOTO ENC: {ver_photo_enc}")
 
-    # get locations of loaded verification photo used for comparison
+    # obtener las ubicaciones de la foto de verificación cargada utilizada para la comparación
     ver_locs = face_recognition.face_locations(ver_photo)
-    # find faces and encodings
+    # encontrar las caras y los encodings
     ver_photo_enc = face_recognition.face_encodings(ver_photo, ver_locs)
 
-    # check if the verification photo matches a user photo
+    # comprobar si la foto de verificación coincide con una foto de usuario
     for (top, right, bottom, left), ver_photo_enc in zip(ver_locs, ver_photo_enc):
         matches = face_recognition.compare_faces(user_photo, ver_photo_enc)
 
-    # return results
+    # Mostrar si se logea o no
     if True in matches:
         print("Thank you for logging in")
         return True
