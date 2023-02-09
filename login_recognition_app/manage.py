c9fadd80dc27bf0497f1fbb8cbf09f57996ed293
Stephany Valderrama Carmona <svalderrama4@gmail.com>
2023-02-09 11:14:12 +0100

    setup Django file, config /Login/Registro/Base de datos~ DB/Images ~media/

--- login_recognition_app/manage.py
+++ login_recognition_app/manage.py
@@ -1,18 +1,21 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Este es el archivo principal para integrar todo el funcionamiento
+de Django con el modelo de face_recognition"""
+
+#Importar librerías y paquetes
+
 import os
 import sys
 
-
+"""Línea de comandos de Django para tareas administrativas."""
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facialrecognition.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+             "Instalar Django "
+            "Activar entorno virtual e interprete "
+
         ) from exc
     execute_from_command_line(sys.argv)
 
