615eeb35ff833e4372ff8c20f7072cd40da6321f
Stephany Valderrama Carmona <svalderrama4@gmail.com>
2023-02-09 13:06:16 +0100

    Configuración WSGI para comunicación HTTP integrada de Django

--- login_recognition_app/facialrecognition/wsgi.py
+++ login_recognition_app/facialrecognition/wsgi.py
@@ -1,15 +1,21 @@
 """
-WSGI config for facialrecognition project.
+Configuración WSGI
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+ WSGI--> Web Server Gateway InterfaceEs un estandar permite a Django comunicarse a través del protocolo HTTP
 
-For more information on this file, see
+Esta configuración queda nombrada en el módulo ``application``.
+
+Mirar la docu:
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+"""Por default se importa de django el parametro env.
+ --> es un diccionarion el cual contiene variables wsgi 
+relacionadas con la petición del cliente (Método del protocolo, Query String etc ...)"""
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facialrecognition.settings')
 
