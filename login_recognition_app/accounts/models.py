c9a4247855da4e18dfa055b18261322791a3e7ee
Stephany Valderrama Carmona <svalderrama4@gmail.com>
2023-02-09 14:51:47 +0100

    Guardar las imágenes desde el USUARIO en la carpeta \media\images

--- login_recognition_app/accounts/models.py
+++ login_recognition_app/accounts/models.py
@@ -1,8 +1,12 @@
+
+# Guardar las imágenes desde el USUARIO
+# Se aplica a la clase UserProfile
+
 from django.db import models
 
 class UserProfile(models.Model):
     title = models.CharField(max_length=25, default='NULL USER')
-    img = models.ImageField(upload_to="images/", default='null.jpg')
+    img = models.ImageField(upload_to="images/", default='null.jpg') # las imagenes se subiran a la carpeta \media\images
 
 
     def __str__(self):
