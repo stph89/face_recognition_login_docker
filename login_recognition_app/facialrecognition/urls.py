dde7f599d0d89da2d3f9d5227c2a20553f4d5016
Stephany Valderrama Carmona <svalderrama4@gmail.com>
2023-02-09 12:47:51 +0100

    Guardar imagenes en carpeta/localhost redirigir en runserver/ navegacion

--- login_recognition_app/facialrecognition/urls.py
+++ login_recognition_app/facialrecognition/urls.py
@@ -1,17 +1,10 @@
-"""facialrecognition URL Configuration
+"""Configuración URL de la app
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+Guardar imagenes en carpeta/localhost redirigir en runserver/ navegacion
+
+--->Documentación para la configuración de URL:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
 from django.contrib import admin
 from django.urls import path, include
