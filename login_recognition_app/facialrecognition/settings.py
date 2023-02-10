6756c8f094dd48204095b2bc4050e2aaa3a3ce0a
wenlla <penelope306115257@gmail.com>
2023-02-10 10:46:06 +0100

    add foto

--- login_recognition_app/facialrecognition/settings.py
+++ login_recognition_app/facialrecognition/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static').replace('\\', '/'),
+)
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'facialrecognition/media')
